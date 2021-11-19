c5ac608d4f3d61fb5bb214b8ce0c5a7432b2e1d1
alexgeary37 <alexgeary37@gmail.com>
2021-11-20 12:40:41 +1300

    Create Server class and refactor code in app.js into server

--- app.js
+++ app.js
@@ -1,40 +1,6 @@
 require("dotenv").config();
+const Server = require("./server");
 
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
-const textFileRoutes = require("./routes/textFileRoutes");
-const sessionTemplateRoutes = require("./routes/sessionTemplateRoutes");
-const readingSessionRoutes = require("./routes/readingSessionRoutes");
-const scrollPosEntryRoutes = require("./routes/scrollPosEntryRoutes");
-const defaultRoute = require("./routes/defaultRoute");
-const port = process.env.PORT;
-const mongoUrl = process.env.ATLAS_URI;
+const server = new Server();
 
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    const app = express();
-    app.use(cors());
-    app.use(express.json({ limit: "50mb" }));
-    app.use(express.urlencoded({ extended: false, limit: "50mb" }));
-
-    app.use(textFileRoutes);
-    app.use(sessionTemplateRoutes);
-    app.use(readingSessionRoutes);
-    app.use(scrollPosEntryRoutes);
-    app.use(defaultRoute);
-
-    app.listen(port, () => {
-      console.log("Server running on port: ", port);
-    });
-
-    // Pick up React index.html file
-    // TODO: Make sure this line of code is in the right place for execution order
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  });
+server.listen();
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+const textFileRoutes = require("./routes/textFileRoutes");
+const sessionTemplateRoutes = require("./routes/sessionTemplateRoutes");
+const readingSessionRoutes = require("./routes/readingSessionRoutes");
+const scrollPosEntryRoutes = require("./routes/scrollPosEntryRoutes");
+const defaultRoute = require("./routes/defaultRoute");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.mongoUrl = process.env.ATLAS_URI;
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json({ limit: "50mb" }));
+    this.app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
+    mongoose.connect(this.mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    // Pick up React index.html file
+    this.app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  routes() {
+    this.app.use(textFileRoutes);
+    this.app.use(sessionTemplateRoutes);
+    this.app.use(readingSessionRoutes);
+    this.app.use(scrollPosEntryRoutes);
+    this.app.use(defaultRoute);
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server running on port: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
