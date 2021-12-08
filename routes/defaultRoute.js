9e2341b3295413f0377e7e808fb0b540d6fa365d
alexgeary37 <alexgeary37@gmail.com>
2021-12-09 11:27:13 +1300

    Import path into defaultRoute.js

--- routes/defaultRoute.js
+++ routes/defaultRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 // Catch all requests that don't match any route
 router.get("*", (req, res) => {
