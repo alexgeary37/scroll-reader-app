658c807e47cb0202f481c02f3f334fdc28f82ba0
alexgeary37 <alexgeary37@gmail.com>
2022-01-27 14:25:39 +1300

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { check } = require("express-validator");
-const { validateInput } = require("../middleware/validate-input");
 const { login } = require("../controllers/auth");
 
 router.post("/api/login", async (req, res) => {
   check("email", "Email is required").isEmail();
   check("password", "Password is required").not().isEmpty();
-  // validateInput(req, res);
   login(req, res);
 });
 
 module.exports = router;
-
-// [
-//   check("email", "Email is required").isEmail(),
-//   check("password", "Password is required").not().isEmpty(),
-//   validateInput,
-// ],
-// login
