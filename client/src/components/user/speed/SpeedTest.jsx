292195dd16c0d46dcb2743562b045e937b16cf2f
alexgeary37 <alexgeary37@gmail.com>
2022-02-02 11:12:56 +1300

    Update

--- client/src/components/user/speed/SpeedTest.jsx
+++ client/src/components/user/speed/SpeedTest.jsx
@@ -200,7 +200,7 @@ const SpeedTest = () => {
     if (sessionContext.hasStartedReading) {
       if (isMobile) {
         return (
-          <div style={{ marginTop: 20 }}>
+          <div style={{ marginTop: 50 }}>
             <SpeedText
               fileID={currentText.fileID}
               textStyleID={currentText.styleID}
