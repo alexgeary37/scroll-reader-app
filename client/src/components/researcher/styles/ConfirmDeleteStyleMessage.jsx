2cf587f490509888ea08505627597c22ba9d5f70
alexgeary37 <alexgeary37@gmail.com>
2022-02-15 18:32:36 +1300

    Create ConfirmDeleteStyleMessage.jsx

---
+++ client/src/components/researcher/styles/ConfirmDeleteStyleMessage.jsx
@@ -0,0 +1,22 @@
+import { Modal, Button } from "semantic-ui-react";
+
+const ConfirmDeleteStyleMessage = ({ isOpen, answerYes, answerNo }) => {
+  return (
+    <Modal
+      size="tiny"
+      open={isOpen}
+      style={{ textAlign: "center", padding: 10 }}
+    >
+      <Modal.Description
+        as="h4"
+        content="Are you sure you want to delete this style?"
+      />
+      <div style={{ marginTop: 10 }}>
+        <Button content="No" onClick={answerNo} />
+        <Button primary content="Yes" onClick={answerYes} />
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmDeleteStyleMessage;
