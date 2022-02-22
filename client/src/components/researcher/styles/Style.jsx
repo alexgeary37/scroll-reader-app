80f3d52f2150af48118fee8a789e140c7bc76460
alexgeary37 <alexgeary37@gmail.com>
2022-02-23 12:54:54 +1300

    Prevent default styles from being removed

--- client/src/components/researcher/styles/Style.jsx
+++ client/src/components/researcher/styles/Style.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import { Item, Button } from "semantic-ui-react";
 import ConfirmDeleteStyleMessage from "./ConfirmDeleteStyleMessage";
 
+const styles = [
+  `{fontFamily: "Times, 'Times New Roman', Georgia, serif", fontSize: "36px"}`,
+  `{fontFamily: "Times, 'Times New Roman', Georgia, serif", fontSize: "30px"}`,
+  `{fontFamily: "Times, 'Times New Roman', Georgia, serif", fontSize: "24px"}`,
+  `{fontFamily: "Times, 'Times New Roman', Georgia, serif", fontSize: "15px"}`,
+  `{fontFamily: "Helvetica, 'Helvetica Neue', Arial, Verdana, sans-serif", fontSize: "15px"}`,
+];
+
 const Style = ({ style, deleteStyle }) => {
   const [openConfirmDeleteStyleMessage, setOpenConfirmDeleteStyleMessage] =
     useState(false);
@@ -13,11 +21,7 @@ const Style = ({ style, deleteStyle }) => {
           <Item.Description content={`${style.style}`} />
           <Button
             floated="right"
-            disabled={
-              style.style === `Times, "Times New Roman", Georgia, serif` ||
-              style.style ===
-                `Helvetica, "Helvetica Neue", Arial, Verdana, sans-serif`
-            }
+            disabled={styles.includes(style.style)}
             content="Remove"
             onClick={() => setOpenConfirmDeleteStyleMessage(true)}
           />
